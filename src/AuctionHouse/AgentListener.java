efbd3c6f886325e06bdb27c85a762df241a60de7
Andrew Geyko <ageyko@unm.edu>
2022-04-30 18:10:35 -0600

    Expanded agent bidding / auction protocol. Added minor testing features to Message, functionality unchanged

--- src/AuctionHouse/AgentListener.java
+++ src/AuctionHouse/AgentListener.java
@@ -3,6 +3,7 @@ package AuctionHouse;
 import util.Message;
 
 import java.io.ObjectInputStream;
+import java.util.Scanner;
 
 /**
  * Class responsible for receiving messages from Agent
@@ -17,6 +18,13 @@ public class AgentListener implements Runnable {
         this.proxy = proxy;
     }
 
+    private void parseBid(Message message) {
+        Scanner sc = new Scanner(message.body);
+        Integer amount = Integer.parseInt(sc.nextLine());
+        Integer auctionID = Integer.parseInt(sc.nextLine());
+        proxy.makeBid(amount, auctionID);
+    }
+
     /**
      * Listen for messages from agent socket and communicate with proxy as
      * necessary
@@ -26,9 +34,9 @@ public class AgentListener implements Runnable {
         try {
             while (true) {
                 Message message = (Message)in.readObject();
-                switch(message.getBody().toLowerCase()) {
+                switch(message.getType().toLowerCase()) {
                     case "get" -> proxy.sendItems();
-                    case "bid" -> proxy.makeBid(100, 0);
+                    case "bid" -> parseBid(message);
                 }
             }
         }
--- src/util/Message.java
+++ src/util/Message.java
@@ -5,9 +5,9 @@ import util.MessageEnums.*;
 public class Message implements Serializable {
     public Origin origin = null;
     public MessageEnums message;
-    private String type = null;
-    private String body = null;
-    private Object proxy = null;
+    public String type = null;
+    public String body = null;
+    public Object proxy = null;
 
     public Message(Origin origin, String type, String body) {
         this.origin = origin;
@@ -33,5 +33,10 @@ public class Message implements Serializable {
 
     public void setProxy(Object proxy) {
         this.proxy = proxy;
+    }
+
+    @Override
+    public String toString() {
+        return "Message from " + origin + " with type " + type + " and body " + body;
     }
 }
